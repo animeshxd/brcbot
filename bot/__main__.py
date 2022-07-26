d17210b9f779c8770792ae7fbd80b6a4f47d3c5f
Animesh Murmu <am2646374@gmail.com>
2022-07-26 12:12:16 +0530

    Update: added scheduler and MongoStorage

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,23 @@
 import asyncio
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from bot.mongo import mongo
+from bot.services.scheduler import scheduler
+from pyromongo.pyromongo import MongoStorage
+
 if __name__ == '__main__':
     from bot import client, notice
     import bot.events.home
     import bot.events.menu
     import bot.events.settings
     import bot.events.notices
-    import bot.events.next
     import bot.events.about
+
+    client.storage = MongoStorage(mongo.db)
+    sched = AsyncIOScheduler()
+    sched.add_job(scheduler, 'interval', hours=5, seconds=10, kwargs=dict(client=client, mongo=mongo, notices=notice, _scheduler=sched))
+    sched.start()
     try:
         client.run(notice.init())
         client.run()
