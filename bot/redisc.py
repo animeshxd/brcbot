a700cf366f47c751b306a5a2ddc2a9524254cf33
Animesh Murmu <am2646374@gmail.com>
2022-09-10 09:26:10 +0530

    Update: REDIS config from env

--- bot/redisc.py
+++ bot/redisc.py
@@ -1,4 +1,10 @@
 import redis.asyncio as redis
 from redis.client import Redis
+import config
 
-rd = redis.Redis(host="localhost")  # type: Redis
+rd = redis.Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    username=config.REDIS_USERNAME,
+    password=config.REDIS_PASSWORD
+)  # type: Redis
--- config.py
+++ config.py
@@ -6,3 +6,7 @@ API_HASH: str = os.getenv('API_HASH')
 BOT_TOKEN: str = os.getenv('BOT_TOKEN')
 MONGO_SRV: str = os.getenv('MONGO_SRV')
 LOG_LEVEL: int = getattr(logging, os.getenv('LOG_LEVEL', 'INFO'), logging.INFO)
+
+REDIS_HOST, REDIS_PORT = os.getenv("REDIS_URL").split(":")
+REDIS_USERNAME: str = os.getenv("REDIS_USERNAME", "default")
+REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD")
