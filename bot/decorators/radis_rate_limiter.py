2c30bfc00d029cb83b02dfe71b73312fce34bbbf
Animesh Murmu <am2646374@gmail.com>
2022-09-12 08:35:08 +0530

    Update: show ttl on timeout

--- bot/decorators/radis_rate_limiter.py
+++ bot/decorators/radis_rate_limiter.py
@@ -18,8 +18,9 @@ def ratelimited(limit=LIMIT, expires=EXPIRE):
             if r == 1:
                 await rd.expire(_m.chat.id, time=expires)
             if r > limit:
+                ttl = await rd.ttl(_m.chat.id)
                 logging.debug(f"request times {r} exceed for user {_m.chat.id}")
-                await _m.reply(f"Too many requests within {expires.seconds} seconds")
+                await _m.reply(f"Too many requests within {expires.seconds} seconds, wait {ttl} seconds")
                 return
             logging.debug(f"request times {r} for user {_m.chat.id}")
             await func(_c, _m, *args, **kwargs)
