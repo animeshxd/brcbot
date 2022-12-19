420af0d611e18c819cab1ea49795d3ee72c4aa50
Animesh Murmu <am2646374@gmail.com>
2022-12-19 18:49:38 +0530

    Update: Specified explicit exports

--- config.py
+++ config.py
@@ -9,4 +9,7 @@ LOG_LEVEL: int = getattr(logging, os.getenv('LOG_LEVEL', 'INFO'), logging.INFO)
 
 REDIS_HOST, REDIS_PORT = os.getenv("REDIS_URL").split(":")
 REDIS_USERNAME: str = os.getenv("REDIS_USERNAME", "default")
-REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD")
+REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "")
+
+__all__ = ["API_ID", "API_HASH", "BOT_TOKEN", "MONGO_SRV", "LOG_LEVEL", "REDIS_HOST", "REDIS_PORT", "REDIS_USERNAME",
+           "REDIS_PASSWORD"]
