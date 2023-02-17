9fca44bbcbf5343f56d8e5313915146fbe42b933
Animesh Murmu <am2646374@gmail.com>
2023-02-17 12:45:01 +0530

    added dotenv

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import logging
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 API_ID: int = int(os.getenv('API_ID'))
 API_HASH: str = os.getenv('API_HASH')
 BOT_TOKEN: str = os.getenv('BOT_TOKEN')
@@ -10,7 +14,7 @@ LOG_LEVEL: int = getattr(logging, os.getenv('LOG_LEVEL', 'INFO'), logging.INFO)
 REDIS_HOST, REDIS_PORT = os.getenv("REDIS_URL").split(":")
 REDIS_USERNAME: str = os.getenv("REDIS_USERNAME", "default")
 REDIS_PASSWORD: str = os.getenv("REDIS_PASSWORD", "")
-PROFILE: int = int(os.getenv("PROFILE", "DEVELOPMENT"))
+PROFILE: int = int(os.getenv("PROFILE", "4"))
 
 __all__ = ["API_ID", "API_HASH", "BOT_TOKEN", "MONGO_SRV", "LOG_LEVEL", "REDIS_HOST", "REDIS_PORT", "REDIS_USERNAME",
            "REDIS_PASSWORD", "PROFILE"]
