376378006ce637166fa12b1c4206e0510d471b62
Animesh Murmu <am2646374@gmail.com>
2023-02-28 17:18:32 +0530

    moved brc/parser to ../parser

--- bot/services/notice/brc/base/__init__.py
+++ bot/services/notice/brc/base/__init__.py
@@ -1,4 +1,5 @@
-from bot.services.notice.brc import raw, parser
+from bot.services.notice import parser
+from bot.services.notice.brc import raw
 
 headers: dict = parser.parse_headers(raw.raw_headers)
 req_data: dict = parser.parse_requests_data(raw.raw_data)
